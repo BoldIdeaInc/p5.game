1cb29ab6e4e43360f08aa8e525bdc5e5ab4533eb
Ben Davis <bendavis78@gmail.com>
2021-06-14 14:15:29 -0500

    updated gh-pages, fixed asteroidHome.js

--- asteroidsHome.js
+++ asteroidsHome.js
@@ -8,29 +8,28 @@ var shipImage, bulletImage, particleImage;
 var MARGIN = 40;
 
 function setup() {
-var canv = createCanvas(windowWidth, windowHeight);
-canv.parent("bgCanvas");
-  
-bulletImage = loadImage("images/asteroids_bullet.png");
-shipImage = loadImage("images/asteroids_ship0001.png");
-particleImage = loadImage("images/asteroids_particle.png");
+  var canv = createCanvas(windowWidth, windowHeight);
+  canv.parent("bgCanvas");
+    
+  bulletImage = loadImage("images/asteroids_bullet.png");
+  shipImage = loadImage("images/asteroids_ship0001.png");
+  particleImage = loadImage("images/asteroids_particle.png");
 
-ship = createSprite(width/2, height/2);
-ship.maxSpeed = 6;
-ship.friction = 0.02;
-ship.setCollider("circle", 0,0, 20);
+  ship = createSprite(shipImage, width/2, height/2);
+  ship.maxSpeed = 6;
+  ship.friction = 0.02;
+  ship.setCollider("circle", 0,0, 20);
 
-ship.addImage("normal", shipImage);
-ship.addAnimation("thrust", "images/asteroids_ship0002.png", "images/asteroids_ship0007.png");
+  ship.addAnimation("thrust", "images/asteroids_ship0002.png", "images/asteroids_ship0007.png");
 
-asteroids = new Group();
-bullets = new Group();
+  asteroids = new Group();
+  bullets = new Group();
 
-for(var i = 0; i<8; i++) {
-  var ang = random(360);
-  var px = width/2 + 1000 * cos(radians(ang));
-  var py = height/2+ 1000 * sin(radians(ang));
-  createAsteroid(3, px, py);
+  for(var i = 0; i<8; i++) {
+    var ang = random(360);
+    var px = width/2 + 1000 * cos(radians(ang));
+    var py = height/2+ 1000 * sin(radians(ang));
+    createAsteroid(3, px, py);
   }
 }
 
@@ -43,11 +42,11 @@ function draw() {
   text("W + A + D keys to move. K to shoot", width-30, 30);
   
   for(var i=0; i<allSprites.length; i++) {
-  var s = allSprites[i];
-  if(s.position.x<-MARGIN) s.position.x = width+MARGIN;
-  if(s.position.x>width+MARGIN) s.position.x = -MARGIN;
-  if(s.position.y<-MARGIN) s.position.y = height+MARGIN;
-  if(s.position.y>height+MARGIN) s.position.y = -MARGIN;
+    var s = allSprites[i];
+    if(s.position.x<-MARGIN) s.position.x = width+MARGIN;
+    if(s.position.x>width+MARGIN) s.position.x = -MARGIN;
+    if(s.position.y<-MARGIN) s.position.y = height+MARGIN;
+    if(s.position.y>height+MARGIN) s.position.y = -MARGIN;
   }
   
   asteroids.overlap(bullets, asteroidHit);
@@ -58,32 +57,27 @@ function draw() {
     ship.rotation -= 4;
   if(keyDown("D"))
     ship.rotation += 4;
-  if(keyDown("W"))
-    {
-    ship.addSpeed(.2, ship.rotation);
+  if(keyDown("W")) {
+    ship.addVelocity(.2, ship.rotation);
     ship.changeAnimation("thrust");
     }
   else
     ship.changeAnimation("normal");
     
-  if(keyWentDown("k"))
-    {
-    var bullet = createSprite(ship.position.x, ship.position.y);
-    bullet.addImage(bulletImage);
-    bullet.setSpeed(10+ship.getSpeed(), ship.rotation);
+  if(keyWentDown("k")) {
+    var bullet = createSprite(bulletImage, ship.position.x, ship.position.y);
+    bullet.setVelocity(10+ship.getSpeed(), ship.rotation);
     bullet.life = 30;
     bullets.add(bullet);
-    }
+  }
   
   drawSprites();
-  
 }
 
 function createAsteroid(type, x, y) {
-  var a = createSprite(x, y);
   var img  = loadImage("images/asteroid"+floor(random(0,3))+".png");
-  a.addImage(img);
-  a.setSpeed(2.5-(type/2), random(360));
+  var a = createSprite(img, x, y);
+  a.setVelocity(2.5-(type/2), random(360));
   a.rotationSpeed = .5;
   //a.debug = true;
   a.type = type;
@@ -100,21 +94,20 @@ function createAsteroid(type, x, y) {
 }
 
 function asteroidHit(asteroid, bullet) {
-var newType = asteroid.type-1;
+  var newType = asteroid.type-1;
 
-if(newType>0) {
-  createAsteroid(newType, asteroid.position.x, asteroid.position.y);
-  createAsteroid(newType, asteroid.position.x, asteroid.position.y);
+  if(newType>0) {
+    createAsteroid(newType, asteroid.position.x, asteroid.position.y);
+    createAsteroid(newType, asteroid.position.x, asteroid.position.y);
   }
 
-for(var i=0; i<10; i++) {
-  var p = createSprite(bullet.position.x, bullet.position.y);
-  p.addImage(particleImage);
-  p.setSpeed(random(3,5), random(360));
-  p.friction = 0.05;
-  p.life = 15;
+  for(var i=0; i<10; i++) {
+    var p = createSprite(particleImage, bullet.position.x, bullet.position.y);
+    p.setVelocity(random(3,5), random(360));
+    p.friction = 0.05;
+    p.life = 15;
   }
 
-bullet.remove();
-asteroid.remove();
+  bullet.remove();
+  asteroid.remove();
 }
--- docs/api.js
+++ docs/api.js
@@ -6,16 +6,16 @@ YUI.add("yuidoc-meta", function(Y) {
         "Group",
         "Sprite",
         "SpriteSheet",
-        "p5.play"
+        "p5.game"
     ],
     "modules": [
-        "p5.play"
+        "p5.game"
     ],
     "allModules": [
         {
-            "displayName": "p5.play",
-            "name": "p5.play",
-            "description": "p5.play is a library for p5.js to facilitate the creation of games and gamelike\nprojects.\n\nIt provides a flexible Sprite class to manage visual objects in 2D space\nand features such as animation support, basic collision detection\nand resolution, mouse and keyboard interactions, and a virtual camera.\n\np5.play is not a box2D-derived physics engine, it doesn't use events, and it's\ndesigned to be understood and possibly modified by intermediate programmers.\n\nSee the examples folder for more info on how to use this library."
+            "displayName": "p5.game",
+            "name": "p5.game",
+            "description": "p5.game is a library for p5.js to facilitate the creation of games and gamelike\nprojects.\n\nIt provides a flexible Sprite class to manage visual objects in 2D space\nand features such as animation support, basic collision detection\nand resolution, mouse and keyboard interactions, and a virtual camera.\n\np5.game is not a box2D-derived physics engine, it doesn't use events, and it's\ndesigned to be understood and possibly modified by intermediate programmers.\n\nSee the examples folder for more info on how to use this library."
         }
     ],
     "elements": []
