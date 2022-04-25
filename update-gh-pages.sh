b133db6c483fdef57053e07c21c3a10e70ed7914
Ben Davis <bendavis78@gmail.com>
2022-04-25 16:16:38 -0500

    Updated gh-pages script to push changes to gh

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -4,29 +4,31 @@ set -eu
 
 echo "Regenerating documentation."
 
-rm -rf docs
-npm run docs
+rm -rf docs &&
+npm run docs &&
 
 echo "Copying/moving files in working dir."
 
-npm run copy:p5
+npm run copy:p5 &&
 rm -rf __temp_examples __temp_docs __temp_p5.game.js
-cp -r examples/ __temp_examples/
+cp -r examples/ __temp_examples/ &&
 mv docs __temp_docs
-cp lib/p5.game.js __temp_p5.game.js
+cp lib/p5.game.js __temp_p5.game.js &&
 
 echo "Switching to gh-pages branch."
 
-git checkout gh-pages
+git checkout gh-pages &&
 
 rm -rf examples docs lib/p5.game.js
-mv __temp_examples examples
-mv __temp_docs docs
-mv __temp_p5.game.js lib/p5.game.js
+mv __temp_examples examples &&
+mv __temp_docs docs &&
+mv __temp_p5.game.js lib/p5.game.js &&
 
 echo "Staging new/changed files."
 
-git add examples/ docs/ lib/p5.game.js
+git add examples/ docs/ lib/p5.game.js &&
 
-echo "Done. Run 'git diff --staged' to review changes."
-echo "If satisfied, run 'git commit' to commit changes."
+git commit -m 'updated gh-pages' &&
+git push origin gh-pages &&
+
+git checkout master
